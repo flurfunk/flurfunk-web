5b90d7fb94f78008e0f5f122bac319460ad134ae
Felix H. Dahlke <fhd@ubercode.de>
2011-08-12 09:28:23 +0200

    Communicate with the real server

--- src/flurfunk/client.cljs
+++ src/flurfunk/client.cljs
@@ -1,13 +1,52 @@
-(ns flurfunk.client)
+(ns flurfunk.client
+  (:require [goog.dom :as dom]
+            [goog.dom.xml :as xml]
+            [goog.net.XhrIo :as XhrIo]))
 
-;; TODO: Get the messages from the server.
+;; TODO: Make it possible to use the stub functions in dev mode.
 
-(def messages [])
+;; TODO: Private
+(def stub-messages [])
 
-(defn get-messages [callback]
-  (callback messages))
+(defn- get-messages-stub [callback]
+  (callback stub-messages))
 
-(defn send-message [message callback]
-  (def messages (conj messages (conj message {:id (. (js/Date.) (getTime))})))
+(defn- send-message-stub [message callback]
+  (def messages (conj stub-messages
+                      (conj message {:id (. (js/Date.) (getTime))})))
   (callback))
 
+(defn- get-request [uri callback]
+  (XhrIo/send uri callback))
+
+(defn- unmarshal-messages [messages]
+  (let [xml (xml/loadXml messages)
+        message-tags (dom/getChildren (.firstChild xml))]
+    (map (fn [message-tag]
+           (let [text (.textContent message-tag)
+                 id (. message-tag (getAttribute "id"))
+                 author (. message-tag (getAttribute "author"))]
+             {:id id :author author :text text}))
+         message-tags)))
+
+(defn- get-messages-http [callback]
+  (get-request "/messages"
+               (fn [e]
+                 (let [target (.target e)
+                       text (. target (getResponseText))]
+                   (callback (unmarshal-messages text))))))
+
+(defn- post-request [uri callback content]
+  (XhrIo/send uri (fn [e] (callback)) "post", content))
+
+(defn- marshal-message [message]
+  (str "<message author='" (:author message) "'>" (:text message) "</message>"))
+
+(defn- send-message-http [message callback]
+  (post-request "/message" (fn [] (callback)) (marshal-message message)))
+
+(defn get-messages [callback]
+  (get-messages-http callback))
+
+(defn send-message [message callback]
+  (send-message-http message callback))
