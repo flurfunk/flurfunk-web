4db365302be3204ea3e388940773f0dfa897bfdc
Felix H. Dahlke <fhd@ubercode.de>
2011-08-25 07:00:36 +0200

    Store author name in a cookie

--- src/flurfunk/core.cljs
+++ src/flurfunk/core.cljs
@@ -2,6 +2,7 @@
   (:require [flurfunk.client :as client]
             [flurfunk.dom-helpers :as dom]
             [goog.events :as events]
+            [goog.net.Cookies :as Cookies]
             [goog.ui.Button :as Button]
             [goog.ui.Container :as Container]
             [goog.ui.Control :as Control]))
@@ -44,25 +45,44 @@
        (. message-textarea (focus))
        (update-message-container message-container)))))
 
+(defn- update-send-button [send-button]
+  (let [author (.value (dom/get-element :author-name-input))
+        text (.value (dom/get-element :message-textarea))]
+    (.setEnabled send-button (not (or (empty? author)
+                                      (empty? text))))))
+
+(defn- set-author-cookie [author]
+  (let [cookies (goog.net/Cookies. js/document)
+        current-time (. (js/Date.) (getTime))
+        expiry-time (+ current-time (* 365 (* 24 (* 60 (* 1000 60)))))]
+    (.set cookies "author" author expiry-time)))
+
+(defn- get-author-cookie []
+  (let [cookies (goog.net/Cookies. js/document)]
+    (.get cookies "author")))
+
 (defn -main []
   (dom/append document.body (create-dom))
   (let [send-button (goog.ui/Button. "Send message")
         message-container (goog.ui/Container.)]
     (.decorate send-button (dom/get-element :send-button))
+    (.setEnabled send-button false)
     (events/listen send-button goog.ui.Component/EventType.ACTION
                    (fn [e] (send-message message-container send-button)))
     (.decorate message-container (dom/get-element :message-container))
     (let [author-name-input (dom/get-element :author-name-input)
-          message-textarea (dom/get-element :message-textarea)
-          change-handler
-          (fn [e]
-            (.setEnabled send-button
-                         (not (or (empty? (.value author-name-input))
-                                  (empty? (.value message-textarea))))))]
-      (doseq [element [author-name-input message-textarea]]
-        (events/listen element goog.events/EventType.INPUT change-handler))
-      (.setEnabled send-button false)
-      (. author-name-input (focus)))
+          message-textarea (dom/get-element :message-textarea)]
+      (events/listen author-name-input goog.events/EventType.INPUT
+                     (fn [e]
+                       (set-author-cookie (.value author-name-input))
+                       (update-send-button send-button)))
+      (events/listen message-textarea goog.events/EventType.INPUT
+                     (fn [e] (update-send-button send-button)))
+      (if-let [author (get-author-cookie)]
+        (set! (.value author-name-input) author))
+      (if (empty? (.value author-name-input))
+        (. author-name-input (focus))
+        (. message-textarea (focus))))
     (js/setInterval (fn [] (update-message-container message-container)) 1000)
     (update-message-container message-container)))
 
