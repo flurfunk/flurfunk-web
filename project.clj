172d2ccc04dc2555abae5539b06fa27d1f26d939
Felix H. Dahlke <fhd@ubercode.de>
2011-10-11 05:54:28 +0200

    Fix code style

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject de.viaboxx.flurfunk/flurfunk-web "0.1.0-SNAPSHOT"
   :description "Web interface for Flurfunk"
   :omit-default-repositories true
-  :repositories {"releases"
-                 {:url "https://www.viaboxxsystems.de/nexus/content/groups/public/"}
-                 "snapshots"
-                 {:url "https://www.viaboxxsystems.de/nexus/content/groups/public/"}})
+  :repositories
+  {"releases"
+   {:url "https://www.viaboxxsystems.de/nexus/content/groups/public/"}
+   "snapshots"
+   {:url "https://www.viaboxxsystems.de/nexus/content/groups/public/"}})
