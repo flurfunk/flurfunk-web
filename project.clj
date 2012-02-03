14795083d732edbf5469c44732305b04ec0d7200
Felix H. Dahlke <fhd@ubercode.de>
2012-02-03 21:32:27 +0100

    Update to Clojure 1.3.0

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Web interface for Flurfunk"
   :source-path "src/clj"
   :cljs-source-path "src/cljs"
-  :dependencies [[lein-clojurescript "1.0.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [lein-clojurescript "1.0.0-SNAPSHOT"]
                  [compojure/compojure "0.6.5"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
   :ring {:handler flurfunk-web.routes/app}
