dcb93979839b0a64d509629975ab9b69131f1e05
Felix H. Dahlke <fhd@ubercode.de>
2012-03-30 14:24:20 +0200

    Extract index template

--- src/clj/flurfunk/web/views.clj
+++ src/clj/flurfunk/web/views.clj
@@ -1,7 +1,7 @@
 (ns flurfunk.web.views
   (:use [hiccup core page-helpers]))
 
-(defn index []
+(defn- index-template [& body]
   (html5
    [:head
     [:meta {:charset "utf-8"}]
@@ -12,26 +12,29 @@
     (include-css "flurfunk.css")
     [:link {:rel "stylesheet" :type "text/css" :href "flurfunk-mobile.css"
             :media "only screen and (max-device-width: 480px)"}]]
-   [:body
-    [:script "
+   (vec (cons :body body))))
+
+(defn index []
+  (index-template
+   [:script "
 // Use a server on the same host, Jetty or Tomcat.
 var flurfunkServer = location.href.replace(\"index.html\", \"\")
     .replace(\"flurfunk-web\", \"flurfunk-server\").replace(/\\/$/, \"\");
 "]
-    (include-js "flurfunk-web.js")]))
+   (include-js "flurfunk-web.js")))
 
 (defn index-dev []
-  (html5
-   [:head
-    [:meta {:charset "utf-8"}]
-    [:meta {:name "viewport"
-            :content (str "width=device-width,initial-scale=1.0,"
-                          "maximum-scale=1.0,user-scalable=0")}]
-    [:title "Flurfunk"]
-    (include-css "flurfunk.css")
-    [:link {:rel "stylesheet" :type "text/css" :href "flurfunk-mobile.css"
-            :media "only screen and (max-device-width: 480px)"}]
-    [:script "
+  (index-template
+   [:form
+    [:input {:type "checkbox" :id "use-real-server"
+             :onchange "enableUrlInput(this.checked)"}]
+    [:label {:for "use-real-server"} "Use real server"]
+    [:label {:for "server-url"} "URL:"]
+    [:input {:id "server-url" :type "text"}]
+    [:button {:onclick "reloadPage()"} "Reload"]]
+   (include-js "out-dev/goog/base.js")
+   (include-js "flurfunk-web-dev.js")
+   [:script "
 function enableUrlInput(enable) {
     document.getElementById(\"server-url\").disabled = !enable;
 }
@@ -45,18 +48,7 @@ function reloadPage() {
         url += \"#server=\" + document.getElementById(\"server-url\").value;
     window.location.href = url;
 }
-"]]
-   [:body
-    [:form
-     [:input {:type "checkbox" :id "use-real-server"
-              :onchange "enableUrlInput(this.checked)"}]
-     [:label {:for "use-real-server"} "Use real server"]
-     [:label {:for "server-url"} "URL:"]
-     [:input {:id "server-url" :type "text"}]
-     [:button {:onclick "reloadPage()"} "Reload"]]
-    (include-js "out-dev/goog/base.js")
-    (include-js "flurfunk-web-dev.js")
-    [:script "
+
 function getParameter(name) {
     var parameters = location.hash.substring(1).split(\"&\");
     for (var i = 0; i < parameters.length; i++) {
@@ -76,4 +68,4 @@ if (flurfunkServer) {
     enableUrlInput(false);
 
 goog.require(\"flurfunk.web.core\");
-"]]))
+"]))
