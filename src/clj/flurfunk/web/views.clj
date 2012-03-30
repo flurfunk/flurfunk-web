10a512cde923314ebfad045065ccd2ffbf51b7ab
Felix H. Dahlke <fhd@ubercode.de>
2012-03-30 10:46:04 +0200

    Add index meta tags

--- src/clj/flurfunk/web/views.clj
+++ src/clj/flurfunk/web/views.clj
@@ -4,6 +4,10 @@
 (defn index []
   (html5
    [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport"
+            :content (str "width=device-width,initial-scale=1.0,"
+                          "maximum-scale=1.0,user-scalable=0")}]
     [:title "Flurfunk"]
     (include-css "flurfunk.css")
     [:link {:rel "stylesheet" :type "text/css" :href "flurfunk-mobile.css"
