bd686f4ec7a90b4010b32e9eaba620fd108bb911
Felix H. Dahlke <fhd@ubercode.de>
2012-04-07 10:01:55 +0200

    Make Leiningen 2 work

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject de.viaboxx.flurfunk/flurfunk-web "0.1.0-SNAPSHOT"
   :description "Web interface for Flurfunk"
-  :source-path "src/clj"
+  :source-paths ["src/clj"]
   :cljs-source-path "src/cljs"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojurescript "0.0-993"]
                  [compojure "0.6.5"]
                  [ring/ring-jetty-adapter "0.3.11"]]
-  :dev-dependencies [[lein-ring "0.4.5"]]
-  :plugins [[lein-cljsbuild "0.1.3"]]
+  :plugins [[lein-cljsbuild "0.1.3"]
+            [lein-ring "0.6.3"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild
   {:builds [{:source-path "src/cljs"
