2833dcd3412d3346d31ee909fe666dc7a8f7d88e
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2012-08-21 10:04:36 +0200

    Release 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.viaboxx.flurfunk/flurfunk-web "0.1.0-SNAPSHOT"
+(defproject de.viaboxx.flurfunk/flurfunk-web "1.0"
   :description "Web interface for Flurfunk"
   :source-paths ["src/clj"]
   :cljs-source-path "src/cljs"
