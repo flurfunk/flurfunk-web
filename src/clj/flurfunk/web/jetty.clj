785a700587f536d0498fae8f78a064d5382aeb39
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2012-08-29 10:04:03 +0200

    Print version info when starting
    This is the same that went into flurfunk-server in dc961b4.

--- src/clj/flurfunk/web/jetty.clj
+++ src/clj/flurfunk/web/jetty.clj
@@ -14,9 +14,31 @@
                       "if you want a specific one."))
         default-port)))
 
+(defn- load-props [file-name]
+    (with-open [^java.io.Reader reader (clojure.java.io/reader (clojure.java.io/resource file-name))]
+          (let [props (java.util.Properties.)]
+                  (.load props reader)
+                  (into {} (for [[k v] props] [(keyword k) (str v)])))))
+
+(defn- get-version []
+  (let [meta-file "META-INF/maven/de.viaboxx.flurfunk/flurfunk-web/pom.properties"]
+    (if-let [props (load-props meta-file)]
+             (:version props)
+             "unknown")))
+
+(defn- print-version []
+  (println (str
+             "==========================\n"
+             "Flurfunk Web\n"
+             "==========================\n"
+             "http://flurfunk.github.com\n"
+             "==========================\n"
+             "Starting flurfunk-web:" (get-version))))
+
 (defn -main [& args]
   (let [port (get-port)]
-    (try (run-jetty app {:port (Integer. port)})
-         (catch NumberFormatException e
-           (println (str "Invalid port number: '" port "'"))
-           (System/exit 1)))))
+    (do (print-version)
+      (try (run-jetty app {:port (Integer. port)})
+        (catch NumberFormatException e
+          (println (str "Invalid port number: '" port "'"))
+          (System/exit 1))))))
